96ea2d94c343e60e15fa875d94ba17fd7f9eec85
Andsirpa <andsirpa94@gmail.com>
2024-05-09 17:15:46 +0200

    feat: add advancedSearch

---
+++ src/components/AdvancedSearch.vue
@@ -0,0 +1,25 @@
+<script></script>
+<template>
+    <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
+        aria-controls="offcanvasExample">
+        Button with data-bs-target
+    </button>
+
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <div>
+                Cerca l'alloggio che fa per te!
+            </div>
+            <div class="mt-3">
+                <form>
+                    <input type="text">
+                </form>
+
+            </div>
+        </div>
+    </div>
+</template>
