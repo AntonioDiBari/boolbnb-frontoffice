7a703af25768f9fc20bbcc30b8ba0c7d7b3bdaa8
Andsirpa <andsirpa94@gmail.com>
2024-05-10 15:09:06 +0200

    add ApartmentDetail

--- src/router.js
+++ src/router.js
@@ -3,20 +3,27 @@ import { createRouter, createWebHistory } from "vue-router";
 import ApartmentList from "./pages/ApartmentList.vue";
 import AppMain from "./components/AppMain.vue";
 import AdvancedSearch from "./components/AdvancedSearch.vue";
+import ApartmentDetail from "./pages/ApartmentDetail.vue";
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: "/",
-      name: "main",
-      component: AppMain,
-    },
-    {
-      path: "/apartment-search",
-      name: "apartment-search",
-      component: ApartmentList,
-    },
-  ],
+    history: createWebHistory(),
+    routes: [
+        {
+            path: "/",
+            name: "main",
+            component: AppMain,
+        },
+        {
+            path: "/apartment-search",
+            name: "apartment-search",
+            component: ApartmentList,
+        },
+        {
+            path: "/apartments/:id",
+            name: "apartment-detail",
+            component: ApartmentDetail,
+            props: true
+        }
+    ],
 });
 export { router };
